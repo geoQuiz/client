c1d117939bf7ccb634bf8a9ea0dc9f2f61a042f1
maharama <you@example.com>
2018-07-26 19:54:45 +0700

    dummy data added in store.js

--- geoquiz/src/store.js
+++ geoquiz/src/store.js
@@ -5,7 +5,41 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    players: [],
+    quiz: [{
+      question: "Sebutkan ibukota turki!",
+      answer: "ankara"
+    },{
+      question: "Di Indonesia, pulau apakah yang berbentuk seperti huruf 'k'?",
+      answer: "sulawesi"
+    },{
+      question: "Dimanakah ibukota Australia?",
+      answer: "canberra"
+    },{
+      question: "Di pulau manakah kota Surabaya?",
+      answer: "jawa"
+    },{
+      question: "Pantai Gading terletak di benua...",
+      answer: "afrika"
+    },{
+      question: "Ottawa adalah ibukota dari negara...",
+      answer: "kanada"
+    },{
+      question: "Moskwa atau Moskow, adalah ibukota dari negara...",
+      answer: "rusia"
+    },{
+      question: "Dimanakah ibukota Brazil?",
+      answer: "brasilia"
+    },{
+      question: "Di kota manakah Candi Borobudur terletak?",
+      answer: "magelang"
+    },{
+      question: "Di negara manakah Menara Pisa berada?",
+      answer: "italia"
+    },{
+      question: "Machu Picchu terletak di negara...",
+      answer: "peru"
+    }]
   },
   mutations: {
 
