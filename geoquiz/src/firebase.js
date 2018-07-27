4f18e258c15f5e5790641d7f2634b5dbd38357a5
Richardson12345 <chuddywarrior@gmail.com>
2018-07-27 10:30:46 +0700

    succcesfully implemented firebase

---
+++ geoquiz/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDExrq3jU0Azk941S5dC5Cfc5IQm9o-AVM",
+    authDomain: "testingproject-210613.firebaseapp.com",
+    databaseURL: "https://testingproject-210613.firebaseio.com",
+    projectId: "testingproject-210613",
+    storageBucket: "testingproject-210613.appspot.com",
+    messagingSenderId: "652934602286"
+  };
+
+  export const app = firebase.initializeApp(config);
+  export const database = firebase.database();
+
+
--- geoquiz/src/views/Home.vue
+++ geoquiz/src/views/Home.vue
@@ -8,6 +8,7 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+// import {app} from '@/firebase.js'
 
 export default {
   name: 'home',
